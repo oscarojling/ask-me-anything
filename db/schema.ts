59f5a4a3e01dfdbeba8ca2e6a811707d67ec537a
Oscar Öjling <oscarojling@gmail.com>
2026-09-08 10:17:16 +0200

    Fix error

--- db/schema.ts
+++ db/schema.ts
@@ -49,6 +49,12 @@ export const account = pgTable("account", {
   providerId: text("provider_id").notNull(),
   issuer: text("issuer"),
   password: text("password"),
+  accessToken: text("access_token"),
+  refreshToken: text("refresh_token"),
+  idToken: text("id_token"),
+  accessTokenExpiresAt: timestamp("access_token_expires_at"),
+  refreshTokenExpiresAt: timestamp("refresh_token_expires_at"),
+  scope: text("scope"),
   createdAt: timestamp("created_at").notNull().defaultNow(),
   updatedAt: timestamp("updated_at").notNull().defaultNow(),
 });
@@ -59,4 +65,5 @@ export const verification = pgTable("verification", {
   value: text("value").notNull(),
   expiresAt: timestamp("expires_at").notNull(),
   createdAt: timestamp("created_at").defaultNow(),
+  updatedAt: timestamp("updated_at").defaultNow(),
 });
